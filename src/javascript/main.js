fcf6658b0de8ed01d3b84d22cbd79d697e206db5
boribs <christumer@gmail.com>
2024-12-07 20:50:38 -0600

    Área de escritura del chat

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,2 +1,12 @@
 // expressjs stuff
 // server invocation
+
+function handleSubmit(e) {
+  if (e.key === "Enter") {
+    const text = e.explicitOriginalTarget.value;
+    console.log(text);
+    e.explicitOriginalTarget.value = "";
+  }
+}
+
+document.getElementById("chat-textbox").addEventListener("keydown", handleSubmit);
